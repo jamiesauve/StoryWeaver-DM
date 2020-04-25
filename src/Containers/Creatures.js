90609c2e0422c7a9c64d40a505212ee6c4558445
Jamie Sauve <jamiesauve@zohomail.com>
2020-04-25 13:29:29 -0500

    Make the Creatures column scrollable

--- src/Containers/Creatures.js
+++ src/Containers/Creatures.js
@@ -2,8 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Section from '../Components/Layout/Section'
+import ScrollableContainer from '../Components/styled/ScrollableContainer'
 
-const This = styled.div``
+const This = styled.div`
+  flex-grow: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+`
 
 const Creatures = (props) => {
   return (
@@ -11,7 +19,11 @@ const Creatures = (props) => {
       <Section
         title="Creatures"
       >
-        Joblin the Goblin
+        <ScrollableContainer
+          className="scrollableContainer"
+        >
+          Joblin the Goblin
+        </ScrollableContainer>
       </Section>
     </This>
   )
