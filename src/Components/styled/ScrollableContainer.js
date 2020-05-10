ecf0a2ece6dd40aaa88b8c886ed9c334e2370b1b
Jamie Sauve <jamiesauve@zohomail.com>
2020-05-09 22:58:24 -0500

    Add horizontal scrolling support to ScrollableContainer

--- src/Components/styled/ScrollableContainer.js
+++ src/Components/styled/ScrollableContainer.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const ScrollableContainer = styled.div`
 display: flex;
-flex-direction: column;
+flex-direction: ${props => props.flexDirection || `column`};
 
 align-items: stretch;
 flex-grow: 1;
