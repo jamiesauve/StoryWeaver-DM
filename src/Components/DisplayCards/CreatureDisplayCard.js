285f99a5d35ca443164819e69377eb8d95ea7bf1
Jamie Sauve <jamiesauve@zohomail.com>
2020-05-09 23:01:14 -0500

    Finish MVP of CreatureDisplayCard
    - reorganize some items for visual cleanliness
    - add actions, damage stats and special abilities sections
    - make sure damage stats can scroll horizontally if need be

--- src/Components/DisplayCards/CreatureDisplayCard.js
+++ src/Components/DisplayCards/CreatureDisplayCard.js
@@ -13,12 +13,15 @@ import Description from '../Stats/Description'
 
 import restructureData from '../SearchResults/dataRestructuring/creature'
 
+import ScrollableContainer from '../styled/ScrollableContainer'
+import Spacer from '../styled/Spacer'
 import DisplayCard from '../styled/DisplayCard/Card'
 import Row from '../styled/DisplayCard/Row'
 import Title from '../styled/DisplayCard/Title'
 import TitleDetail from '../styled/DisplayCard/TitleDetail'
 
 import colors from '../../data/colors'
+import BulletList from '../Stats/BulletList'
 
 const This = styled.div``
 
@@ -26,29 +29,18 @@ const Filler = styled.div`
   flex-grow: 1;
 `
 
-const StatBlock = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  flex-grow: 0;
-  flex-shrink: 0;
-`
-
-const StatColumn = styled.div`
+const DamageStatContainer = styled.div`
   flex-grow: 1;
-  
-  display: flex;
-  flex-direction: columm;
-`
+  flex-shrink: 0;
 
-const StatCell = styled.div`
-  width: 65px;
+  max-width: ${props => props.areThereMultipleDamageStats ? `75%` : `100%`};
+
+  display: flex;
+  flex-direction: inherit;
 `
 
 const CreatureDisplayCard = (props) => {
   const creatureData = restructureData(props.data)
-
-  console.log('creatureData', creatureData)
 
   const {
     label,
@@ -68,9 +60,21 @@ const CreatureDisplayCard = (props) => {
     senses,
     size,
     speed,
+    specialAbilities,
+    spellDC,
     subType,
     type,
   } = creatureData
+
+  // used to make sure one damage stat doesn't fill the screen if there are others as it would hide them visually
+  const areThereMultipleDamageStats = _.chain([
+    !_.isEmpty(damageImmunities),
+    !_.isEmpty(damageResistances),
+    !_.isEmpty(damageVulnerabilities)
+  ])
+  .compact()
+  .value()
+  .length > 1
 
   return (
     <This>
@@ -85,47 +89,42 @@ const CreatureDisplayCard = (props) => {
           <TitleDetail>
             {size} {type}{subType && ` (${subType})`}, {alignment}
           </TitleDetail>
+        </Row>
 
+        <Row
+          className="row"
+        >
+          <InlineStat 
+            color={colors.battleRed}
+            label={"CR"}
+            value={challengeRating}
+          />
+        
           <Filler />
 
-          <StatBlock>
-            <StatColumn>
-              <StatCell>
-                <InlineStat 
-                  color={colors.battleRed}
-                  label={"CR"}
-                  value={challengeRating}
-                />
-              </StatCell>
-              
-              <StatCell>
-                <InlineStat 
-                  color={colors.weatherBlue}
-                  label={"AC"}
-                  value={armorClass}
-                />
-              </StatCell>
-            </StatColumn>
+          <InlineStat 
+            color={colors.magicPink}
+            label={"PP"}
+            value={passivePerception}
+          />
+        </Row>
 
-            <StatColumn>
-              <StatCell>
-                <InlineStat 
-                  color={colors.magicPink}
-                  label={"PP"}
-                  value={passivePerception}
-                />
-              </StatCell>
+        <Row>
+          <ScrollableContainer
+            className="scrollableContainer"
+            flexDirection="row"
+          >
+            <Senses
+              data={senses}
+            />
 
-              <StatCell>
-                <InlineStat 
-                  color={colors.puzzleOrange}
-                  label={"HP"}
-                  value={hitPoints}
-                />
-              </StatCell>
-            </StatColumn>
-          </StatBlock>
-          </Row>
+            <Filler />
+
+            <Speed
+              data={speed}
+            />
+          </ScrollableContainer>
+        </Row>
 
         {languages 
           && <Row>
@@ -137,24 +136,11 @@ const CreatureDisplayCard = (props) => {
 
         {!_.isEmpty(proficiencies)
           && <Row>
-          <Proficiencies
-            data={proficiencies}
-          />
-        </Row>
-          
+            <Proficiencies
+              data={proficiencies}
+            />
+          </Row>  
         }
-
-        <Row>
-          <Senses
-            data={senses}
-          />
-
-          <Filler />
-
-          <Speed
-            data={speed}
-          />
-        </Row>
 
         <Row>
           <AbilityScores
@@ -162,9 +148,104 @@ const CreatureDisplayCard = (props) => {
           />
         </Row>
 
-        <Row>
-          hi
+        <Row
+          padding={"5px"}
+        >
+          <Filler />
+
+          {spellDC
+            && <InlineStat 
+            color={colors.magicPink}
+            label={"Spell DC"}
+            value={spellDC}
+          />
+          }
+
+          <InlineStat 
+            color={colors.weatherBlue}
+            label={"AC"}
+            value={armorClass}
+          />
+        
+          <InlineStat 
+            color={colors.puzzleOrange}
+            label={"HP"}
+            value={hitPoints}
+          />
         </Row>
+
+        <Row>
+        <ScrollableContainer
+            className="scrollableContainer"
+            flexDirection="row"
+          >
+            {!_.isEmpty(damageImmunities)
+              && <DamageStatContainer
+                areThereMultipleDamageStats={areThereMultipleDamageStats}
+              >
+                <BulletList
+                  borderColor={colors.battleRed}
+                  items={damageImmunities}
+                  heading="Damage Immunities"
+                />
+              </DamageStatContainer>
+            }
+
+            {!_.isEmpty(damageResistances)
+              && <DamageStatContainer
+                areThereMultipleDamageStats={areThereMultipleDamageStats}
+              >
+                <BulletList
+                  borderColor={colors.puzzleOrange}
+                  items={damageResistances}
+                  heading="Damage Resistances"
+                />
+              </DamageStatContainer>
+            }
+
+            {!_.isEmpty(damageVulnerabilities)
+              && <DamageStatContainer
+                areThereMultipleDamageStats={areThereMultipleDamageStats}
+              >
+                <BulletList
+                  borderColor={colors.forestGreen}
+                  items={damageVulnerabilities}
+                  heading="Damage Vulnerabilities"
+                />
+              </DamageStatContainer>
+            }
+          </ScrollableContainer>
+        </Row>
+
+        {
+          (
+            _.isEmpty(specialAbilities) 
+            && !_.isEmpty(damageImmunities) // don't want two different item types of the same color next to each other
+          )
+        ? <Spacer />
+        : _.map(specialAbilities, specialAbility => (
+          <Description
+            borderColor={colors.magicPink}
+            text={`
+            ${specialAbility.description}
+            ${specialAbility.usage 
+              ? `(${specialAbility.usage.times} times ${specialAbility.usage.type})` 
+              : ``
+            }
+            `}
+            heading={specialAbility.name}
+          />
+        ))}
+
+        {_.map(actions, action => (
+          <Row>
+            <Description
+            borderColor={colors.battleRed}
+            text={action.description}
+            heading={action.name}
+            />
+          </Row>
+        ))}
 
       </DisplayCard>
     </This>
