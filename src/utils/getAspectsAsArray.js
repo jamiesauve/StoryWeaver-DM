3ff30f0cb90684367bc43eb6e6eb4a915a5bd87e
Jamie Sauve <jamiesauve@zohomail.com>
2020-06-14 21:29:45 -0500

    Add data file for aspects - getAspectsAsArray
    - making these exportable as one array will allow them to be
    looped through and used dynamically

---
+++ src/utils/getAspectsAsArray.js
@@ -0,0 +1,46 @@
+// Aspects
+import Ambiences from '../Containers/Ambiences'
+import Creatures from '../Containers/Creatures'
+// Gameplay
+import Notes from '../Containers/Notes'
+// NPCs
+import Reagents from '../Containers/Reagents'
+// Recipes
+import Search from '../Containers/Search'
+
+import colors from '../data/colors'
+
+const Aspects = {
+  "notes": {
+    component: () => Notes,
+    color: colors.villageBrown,
+    name: "notes",
+    label: "Notes",
+  },
+  "ambiences": {
+    component: () => Ambiences,
+    color: colors.magicPink,
+    name: "ambiences",
+    label: "Ambiences",
+  },
+  "reagents": {
+    component: () => Reagents,
+    color: colors.forestGreen,
+    name: "reagents",
+    label: "Reagents",
+  },
+  "search": {
+    component: () => Search,
+    color: colors.creepyPurple,
+    name: "search",
+    label: "Search",
+  },
+  "creatures": {
+    component: () => Creatures,
+    color: colors.puzzleOrange,
+    name: "creatures",
+    label: "Creatures",
+  },
+}
+
+export default Aspects
