88edaeb96ae27c85d49ad056be5d25bb62c21716
Jamie Sauve <jamiesauve@zohomail.com>
2020-06-14 21:25:41 -0500

    Create AspectSlot and AspectTabContainer
    - these two will be used for making the frames dynamic
    - an AspectSlot is one of the visual places on the UI where an
    aspect can show up. It has tabs and different aspects can be shown
    there, hence AspectSlot
    - AspectTabContainer is a wrapper around TabContainer with some
    logic to connect the data in aspects to the TabContainer while
    maintaining TabContainer's reusability.

---
+++ src/Components/MainWindow/AspectSlot.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import _ from 'lodash'
+
+import Frame from './Frame'
+import Pane from './Pane'
+
+import AspectTabContainer from './AspectTabContainer'
+
+const This = styled.div``
+
+const AspectSlot = (props) => {
+  const [activeAspect, setActiveAspect] = useState(null)
+  const [ActiveAspectComponent, setActiveAspectComponent] = useState(null) // a component can't be rendered with a lower-case name, hence this variable
+
+  useEffect(() => {
+    setActiveAspect(aspects[0])
+    setActiveAspectComponent(aspects[0].component)
+  }, [])
+
+  const {
+    activeTerrain,
+    aspects,
+    mainWindowBodyHeight,
+  } = props
+
+  return (
+    <This>
+      <Frame
+          className="frame"
+        >
+          {!_.isNull(activeAspect)
+            && <>
+              <AspectTabContainer
+                className="pane"
+
+                aspects={aspects}
+                activeAspect={activeAspect.name}
+                setActiveAspect={setActiveAspect}
+              />
+
+              <Pane
+                className="pane"
+
+                borderColor={activeAspect.color}
+                // margin, border and padding on pane for both this pane and the MainWindowHeader one
+                height={`${mainWindowBodyHeight - 64}px`} 
+                isBorderTopVisible={false}
+              >
+                {
+                  ActiveAspectComponent !== null
+                  && <ActiveAspectComponent 
+                    activeTerrain={activeTerrain}
+                  />
+                }
+              </Pane> 
+            </>
+          }
+          
+        </Frame>
+    </This>
+  )
+}
+
+export default AspectSlot
---
+++ src/Components/MainWindow/AspectTabContainer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import _ from 'lodash'
+
+import TabContainer from '../UI/TabContainer'
+
+const This = styled.div``
+
+const AspectTabContainer = (props) => {
+  const tabOptions = _.map(props.aspects, aspect => ({
+    label: aspect.label,
+    value: aspect.name,
+  }))
+
+  const handleSetActiveTab = (aspectName) => {
+    const newActiveAspect = _.find(props.aspects, { name: aspectName })
+
+    props.setActiveAspect(newActiveAspect)
+  }
+
+  return (
+    <This>
+      <TabContainer 
+        activeTab={props.activeAspect}
+        tabs={tabOptions}
+        setActiveTab={handleSetActiveTab}
+      />
+    </This>
+  )
+}
+
+export default AspectTabContainer
