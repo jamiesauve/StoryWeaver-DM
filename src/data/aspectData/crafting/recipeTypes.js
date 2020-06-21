348606387c9cd24e8455bd697afbd4f9ccb0efbb
Jamie Sauve <jamiesauve@zohomail.com>
2020-06-21 16:53:07 -0500

    Add recipeTypes data file

---
+++ src/data/aspectData/crafting/recipeTypes.js
@@ -0,0 +1,11 @@
+const recipeTypes = {
+  consumable: "consumable", 
+  cosmetic: "cosmetic", 
+  gear: "gear", 
+  item: "item", 
+  medicine: "medicine", 
+  poison: "poison",
+  other: "other",
+}
+
+export default recipeTypes
