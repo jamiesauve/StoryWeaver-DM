60d4b98081f44f91b40149218d5068440c66fee5
Jamie Sauve <jamiesauve@zohomail.com>
2020-06-27 08:52:45 -0500

    Add Harvesting and HarvestingEntry components
    - these will be used in ReagentDisplayCard

---
+++ src/Components/Crafting/Harvesting.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import _ from 'lodash'
+
+import ColoredBox from '../UI/ColoredBox'
+
+import HarvestingEntry from './HarvestingEntry'
+
+import colors from '../../data/styles/colors'
+import sizes from '../../data/styles/sizes'
+
+const This = styled.div`
+  font-size: ${sizes.small}
+`
+
+const Heading = styled.div`
+  align-self: center;
+
+  font-weight: bold;
+  font-size: ${sizes.small};
+`
+
+const Harvesting = (props) => {
+  const {
+    reagent,
+  } = props
+
+  return (
+    <This>
+      <ColoredBox
+        color={colors.puzzleOrange}
+      >
+        <Heading>
+          Harvesting
+        </Heading>
+
+        {_.map(reagent, harvestingEntry => (
+          <HarvestingEntry
+            harvestingEntry={harvestingEntry}
+            key={harvestingEntry.reagentPart.name}      
+          />
+        ))}
+      </ColoredBox>
+    </This>
+  )
+}
+
+export default Harvesting
---
+++ src/Components/Crafting/HarvestingEntry.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import styled from 'styled-components'
+import _ from 'lodash'
+
+import DataTable from '../Layout/DataTable'
+
+import colors from '../../data/styles/colors'
+
+import shade from '../../utils/shade'
+
+const This = styled.div`
+
+  margin-top: 10px;
+  box-shadow: 0 -1px 1px ${shade(colors.puzzleOrange, -50)};
+  padding-top: 10px;
+`
+
+const ReagentName = styled.div`
+  margin: 0 0 5px 5px;
+  
+  font-weight: bold;
+`
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+
+
+const HarvestingEntry = (props) => {
+  const {
+    harvestingEntry: {
+      reagentPart,
+      harvestDC,
+      onHarvestFailure,
+      recipes,
+      simpleUses,
+      value,
+    },
+  } = props
+
+  const formattedRecipes = _.map(recipes, (recipe, index) => (
+    `${recipe.label} (${recipe.type.mainType} - ${recipe.type.subType})`
+  ))
+
+  const formattedSimpleUses = _.map(simpleUses, simpleUse => (
+    `${simpleUse.mainType} (${simpleUse.subType})`
+  ))
+
+  const harvestingData = [{
+    labelCell: `Value:`,
+    valueCells: [`${value.amount} ${value.unit.shortName} per ${value.amountOfReagent || `ounce`}`],
+  }, {
+    labelCell: `Harvest DC:`,
+    valueCells: [harvestDC],
+  }, {
+    labelCell: `On Fail:`,
+    valueCells: [onHarvestFailure],
+  }, {
+    labelCell: `Raw Uses:`,
+    valueCells: [formattedSimpleUses],
+  }, {
+    labelCell: `Recipes:`,
+    valueCells: [formattedRecipes],
+  }];
+
+  return (
+    <This>
+      <Row>
+        <ReagentName>
+          {reagentPart.label}
+        </ReagentName>
+      </Row>
+
+      <DataTable 
+        title={`Harvesting`}
+        tableRows={harvestingData}
+      />
+    </This>
+  )
+}
+
+export default HarvestingEntry
