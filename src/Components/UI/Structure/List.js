f02aa2411cb3d5ea925d7ba385a8d0c3aa5f2328
Jamie Sauve <jamiesauve@zohomail.com>
2020-06-27 13:14:43 -0500

    Remove two unused imports from List.js

--- src/Components/UI/Structure/List.js
+++ src/Components/UI/Structure/List.js
@@ -2,9 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import _ from 'lodash'
 
-import shadeColor from '../../../utils/shade'
-
-import colors from '../../../data/styles/colors'
 import sizes from '../../../data/styles/sizes'
 
 const This = styled.div`
