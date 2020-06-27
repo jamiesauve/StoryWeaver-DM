0800c2dd95b0fb5a58d755d1d1e9718f98fa4225
Jamie Sauve <jamiesauve@zohomail.com>
2020-06-27 08:47:50 -0500

    Create ColoredBox component
    - this was broken out of a few different components using this
    pattern. This creates a colored background and border around the
    box; it will be much more maintainable to have these styles in one
    place.

---
+++ src/Components/UI/ColoredBox.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import colors from '../../data/styles/colors'
+
+import shade from '../../utils/shade'
+
+const StyledBox = styled.div`
+  flex-grow: 1;
+
+  margin: 2px;
+  border: 1px solid ${props => props.color || colors.lightGrey};
+  border-radius: 5px;
+  padding: 3px;
+  
+  display: flex;
+  flex-direction: column;
+
+  min-width: 30px;
+
+  background: ${props => shade(props.color || colors.lightGrey, -70) || colors.darkBackground};
+  color: ${colors.mediumGrey};
+`
+
+const ColoredBox = (props) => (
+  <StyledBox
+    {...props}
+  >
+    {props.children}
+  </StyledBox>
+)
+
+
+export default ColoredBox
