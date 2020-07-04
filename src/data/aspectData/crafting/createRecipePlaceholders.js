84260bfcabebe4ef55489b199dba4b5ce49d70c9
Jamie Sauve <jamiesauve@zohomail.com>
2020-07-04 15:46:59 -0500

    Make createRecipePlaceholders strings, not arrays
    - since each field in a list would have the same placeholder, it
    only complicates things to return an array of placeholders.

--- src/data/aspectData/crafting/createRecipePlaceholders.js
+++ src/data/aspectData/crafting/createRecipePlaceholders.js
@@ -9,19 +9,13 @@ const createRecipeData = {
   distillingTime: "How long does it need to sit before being used?",
 
   description: "What does the finished product look like, smell like, feel like?",
-  effects: [
-    "What does this creation do, ie. 'This sword does 1d6 fire damage', or 'this potion makes the drinker float a few inches off the ground for 1d6 days'",
-  ],
-  lore: [
-    "Are there any interesting facts about this creation?",
-  ],
-  ingredients: [
-    {
+  effects: "What does this creation do? Ie. 'This sword does 1d6 fire damage', or 'this potion makes the drinker float a few inches off the ground for 1d6 days'",
+  lore: "Are there any interesting facts about this creation?",
+  ingredients: {
       label: "Name of ingredient",
       name: "",
       amount: "1 ounce",
     },
-  ],
   instructions: "How does a creature use the ingredients to make this creation?",
   amountCreated: "1 ounce",
   value: {
