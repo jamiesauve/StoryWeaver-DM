9ed136eefe9a0a1f5e59f0c7987c54069e72b91e
Jamie Sauve <jamiesauve@zohomail.com>
2020-07-29 21:28:12 -0500

    Add a create drawer to Wiki and make the DisplayCard editable

--- src/Components/DisplayCards/WikiEntryDisplayCard.js
+++ src/Components/DisplayCards/WikiEntryDisplayCard.js
@@ -4,19 +4,32 @@ import _ from 'lodash'
 
 import ColoredBox from '../UI/Style/ColoredBox'
 import List from '../UI/Structure/List'
+import EditableList from '../UI/Action/EditableList'
+import EditableInput from '../UI/Action/EditableInput'
 
 import { 
   DisplayCard, 
   Row,
 } from '..//UI/Style/DisplayCard'
 
+import colors from '../../data/styles/colors'
+
 const This = styled.div``
+
+const WikiEntryStatsSection = styled.div`
+  padding: 5px;
+`
 
 const WikiEntryDisplayCard = (props) => {
   const {
-    paragraphs,
-    titleColor,
-  } = props.wikiEntry
+    isEditable,
+    isInCreateMode,
+    placeholders,
+    wikiEntry,
+  } = props
+  
+  const paragraphs = wikiEntry.paragraphs || []
+  const titleColor = wikiEntry.titleColor || colors.mountainsTeal
 
   return (
     <This>
@@ -28,10 +41,70 @@ const WikiEntryDisplayCard = (props) => {
           <ColoredBox
             color={titleColor}
           >
-            <List
+            {
+              isEditable 
+              && <WikiEntryStatsSection>
+                <Row
+                  className="row"
+                  padding="0 0 5px 0"
+                >
+                  <EditableInput
+                    flexGrow={1}
+                    heading="Associated Locations"
+                    placeholder={placeholders.locationTags}
+                    type="text"
+                    value={wikiEntry.locationTags}
+                  />
+                </Row>
+
+                <Row
+                  className="row"
+                  padding="0 0 5px 0"
+                >
+                  <EditableInput // TODO: replace with a select
+                    flexGrow={1}
+                    heading={"Type"}
+                    placeholder={placeholders.type}
+                    type="text"
+                    value={wikiEntry.type}
+                  />
+
+                  <EditableInput // TODO: create a color picker here
+                    alignItems="center"
+                    heading="Color"
+                    placeholder={placeholders.titleColor}
+                    type="text"
+                    value={wikiEntry.titleColor}
+                    inputWidth="20px"
+                  />
+                </Row>
+
+                <Row
+                  className="row"
+                  padding="0 0 5px 0"
+                >
+                  <EditableInput // TODO: replace with a multi
+                    flexGrow={1}
+                    heading="Title"
+                    placeholder={placeholders.label}
+                    type="text"
+                    value={wikiEntry.label}
+                  />
+                </Row>
+              </WikiEntryStatsSection>
+            }
+            {
+              isEditable
+              ? <EditableList 
+                heading="About"
+                items={paragraphs}
+                placeholder={placeholders.paragraphs}
+              />
+              : <List
               items={paragraphs}
               textAlign="left"
             />
+            }
           </ColoredBox>
         </Row>
       </DisplayCard>
