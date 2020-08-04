e599bab92c9aa2f9ac08c91e961efafbb4e1b724
Jamie Sauve <jamiesauve@zohomail.com>
2020-08-03 19:29:20 -0500

    Remove data that doesn't need to be in an open-source app
    - none of this is secret, but I don't want to give the owners/
    creators of any of the tracks that I've been using for my personal
    game a reason to be upset by linking to their work extensively in
    this project. If you are reading this and wish to use tracks from
    Michael Ghelfi or Two Steps From Hell (or others), please support
    them. Michael is a nice guy and doesn't ask for much, and he does
    produce remarkable work.

--- src/data/aspectData/ambiences/specialTracks.js
+++ src/data/aspectData/ambiences/specialTracks.js
@@ -4,15 +4,10 @@ export default [{
   categoryLabel: "Special Events",
   titleColor: colors.specialPink,
   trackObjects: [
-    {
-      title: 'Desert Chase (1:03)',
-      location: 'https://www.youtube.com/watch?v=L65k0_Y-LPY',
-      tags: ['Skyworld', 'wyvern', 'desert'],
-    },
-    {
-      title: 'El Dorado (0:59)',
-      location: 'https://www.youtube.com/watch?v=RSrqK0tU0uc',
-      tags: ['El Dorado', '',],
-    },
+    // {
+    //   title: '',
+    //   location: '',
+    //   tags: [''],
+    // },
   ],
 }]
