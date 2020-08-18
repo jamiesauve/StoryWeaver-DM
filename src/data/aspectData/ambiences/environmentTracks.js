05d844261222f647245f50c5d705368e986d75d9
Jamie Sauve <jamiesauve@zohomail.com>
2020-08-18 08:38:38 -0500

    Revert "Remove data that doesn't need to be in an open-source app"
    This reverts commit e599bab92c9aa2f9ac08c91e961efafbb4e1b724.

--- src/data/aspectData/ambiences/environmentTracks.js
+++ src/data/aspectData/ambiences/environmentTracks.js
@@ -20,6 +20,199 @@ const {
 
 export default [
   {
+    title: 'Horseback - Light Forest',
+    location: 'https://michaelghelfi.bandcamp.com/track/horseback',
+    tags: ['hooves', 'horseback', 'birdsong', 'walk'],
+    terrain: [
+      coast, 
+      forest,
+      jungle,
+      mountains,
+    ]
+  },
+  {
+    title: 'Plains Night (4:03)',
+    location: 'https://www.youtube.com/watch?v=MdZv1YVE6_w&vl=en',
+    tags: ['quiet', 'peaceful', 'night', 'crickets'],
+    terrain: [
+      coast,
+      desert, 
+      mountains, 
+      plains, 
+      swamp, 
+    ],
+  },
+  {
+    title: 'Forest Night (10:00)',
+    location: 'https://www.youtube.com/watch?v=_FJIH0Yi2Mk',
+    tags: ['quiet', 'peaceful', 'night', 'crickets'],
+    terrain: [
+      forest, 
+      jungle,
+      mountains,
+      swamp, 
+    ],
+  },
+  {
+    title: 'Quiet Night River',
+    location: 'https://www.youtube.com/watch?v=ty_Pq__TU_0',
+    tags: ['birds', 'day', 'peaceful'],
+    terrain: [
+      desert, 
+      forest, 
+      freshwater,
+      mountains,
+      plains, 
+      swamp, 
+    ],
+  },
+  {
+    title: 'Cave - inhabited',
+    location: 'https://www.youtube.com/watch?v=CSPjCl_7hXo',
+    tags: ['cave', 'fire', 'underground', 'magic', 'chains'],
+    terrain: [
+      caves,
+    ]
+  },
+  {
+    title: 'Dripping Windy River Cave',
+    location: 'https://www.youtube.com/watch?v=H0cF3t01T-w',
+    tags: ['dripping', 'windy', 'river', 'squeak'],
+    terrain: [
+      caves,
+    ]
+  },
+  {
+    title: 'Crystal Cave',
+    location: 'https://www.youtube.com/watch?v=-ck5wXXD-_4',
+    tags: [
+      'crystal', 'cave', 'underground', 'magical', 'wonder',
+    ],
+    terrain: [
+      caves,
+      winter,
+    ],
+    places: [{
+      name: placeNames.ambalora,
+      trackTitle: 'Ambalora',
+    }]
+  },
+  {
+    title: 'Sea Cave',
+    location: 'https://www.youtube.com/watch?v=onmRPro40GI',
+    tags: ['waves',],
+    terrain: [
+      caves,
+      coast,
+    ]
+  },
+  {
+    title: 'By a Stream',
+    location: 'https://michaelghelfi.bandcamp.com/track/lively-tundra-2',
+    tags: ['stream', 'fire', 'day', 'birds'],
+    terrain: [
+      coast,
+      forest,
+      freshwater,
+      jungle,
+      plains,
+      mountains,
+    ]
+  },
+  {
+    title: 'Calm Market',
+    location: 'https://michaelghelfi.bandcamp.com/track/slums',
+    tags: ['people', 'market', 'busy', 'dogs'],
+    terrain: [
+      city,
+      village
+    ]
+  },
+  {
+    title: 'Cicadas Campfire',
+    location: 'https://michaelghelfi.bandcamp.com/track/campfire-at-dusk',
+    tags: ['campfire', 'cicadas', 'evening', 'peaceful'],
+    terrain: [
+      coast,
+      desert,
+      forest,
+      jungle,
+      mountains,
+      plains,
+      swamp,
+    ]
+  },
+  {
+    title: 'Crickets Campfire',
+    location: 'https://michaelghelfi.bandcamp.com/track/savannah-night-camp',
+    tags: ['campfire', 'crickets', 'birds', 'peaceful'],
+    terrain: [
+      coast,
+      desert,
+      forest,
+      jungle,
+      mountains,
+      plains,
+      swamp,
+    ]
+  },
+  {
+    title: 'Black Market (bubbling)',
+    location: 'https://michaelghelfi.bandcamp.com/track/black-market-2',
+    tags: ['market', 'people',],
+    terrain: [
+      city
+    ]
+  },
+  {
+    title: 'Sewers',
+    location: 'https://www.youtube.com/watch?v=pv7sc85UZ-4',
+    tags: ['sewer', 'dripping', 'caves', 'squeaks'],
+    terrain: [
+      caves,
+    ],
+    places: [{
+      name: placeNames.durukhaaz,
+      trackTitle: "The Lake",
+    }]
+  },
+  {
+    title: 'Digging',
+    location: 'https://michaelghelfi.bandcamp.com/track/construction-site',
+    tags: ['ruins', 'working', 'poeple', 'busy'],
+    terrain: [
+      city,
+      village,
+    ]
+  },
+  {
+    title: 'Swamp',
+    location: 'https://michaelghelfi.bandcamp.com/track/swamp',
+    tags: ['swamp', 'flies', 'crickets'],
+    terrain: [
+      swamp
+    ]
+  },
+  {
+    title: 'Night Swamp - Haunted',
+    location: 'https://michaelghelfi.bandcamp.com/track/dead-marshes',
+    tags: ['swamp', 'flies', 'magic', 'crickets', 'tense'],
+    terrain: [
+      forest,
+      jungle,
+      mountains,
+      swamp
+    ]
+  },
+  {
+    title: 'Swamp Campfire',
+    location: 'https://michaelghelfi.bandcamp.com/track/swamp-campfire',
+    tags: ['campfire', 'crickets', 'flies'],
+    terrain: [
+      swamp,
+    ],
+  },
+  {
     title: 'Quiet Day - Forest',
     location: 'https://www.youtube.com/watch?v=HqdjZSYNV8M',
     tags: ['birds', 'day', 'peaceful'],
@@ -42,6 +235,189 @@ export default [
     ],
   }, 
   {
+    title: 'Daytime Forest (roars)',
+    location: 'https://michaelghelfi.bandcamp.com/track/daytime-forest',
+    tags: ['leaves', 'stream',],
+    terrain: [
+      forest,
+      jungle,
+      mountains,
+    ],
+  },
+  {
+    title: 'Twisted Forest - Roaring',
+    location: 'https://www.youtube.com/watch?v=7ElGv7zMqeY',
+    tags: ['creepy', 'roaring', 'howling', 'forest'],
+    terrain: [
+      caves,
+      forest,
+      jungle,
+      swamp,
+    ]
+  },
+  {
+    title: 'Desert Wind',
+    location: 'https://michaelghelfi.bandcamp.com/track/desert',
+    tags: ['desert', 'wind',],
+    terrain: [
+      desert,
+      mountains,
+      plains,
+      winter,
+    ]
+  },
+  {
+    title: 'Desert Campfire',
+    location: 'https://michaelghelfi.bandcamp.com/track/desert-campfire',
+    tags: ['desert', 'wind',],
+    terrain: [
+      desert,
+      mountains,
+      plains,
+      winter,
+    ]
+  },
+  {
+    title: 'Ruins',
+    location: 'https://michaelghelfi.bandcamp.com/track/ruined-castle-2',
+    tags: ['quiet', 'ruins', 'calm', 'mystery', 'sad', 'desolate'],
+    terrain: [
+      coast,
+      desert,
+      forest,
+      jungle,
+      mountains,
+      plains,
+      winter,
+    ]
+  },
+  {
+    title: 'Night Ruins',
+    location: 'https://michaelghelfi.bandcamp.com/track/nightfall-ruins',
+    tags: ['quiet', 'ruins', 'calm', 'mystery', 'night', 'sad'],
+    terrain: [
+      coast,
+      desert,
+      forest,
+      jungle,
+      mountains,
+      plains,
+      swamp,
+      winter,
+    ]
+  },
+  {
+    title: 'Windy Ocean',
+    location: 'https://michaelghelfi.bandcamp.com/track/windy-coast',
+    tags: ['waves', 'coast', 'wind', 'ocean'],
+    terrain: [
+      coast,
+      ocean,
+    ]
+  },
+  {
+    title: 'Bazaar',
+    location: 'https://michaelghelfi.bandcamp.com/track/bazaar',
+    tags: ['market', 'bazaar', 'busy', 'crowd', 'people', 'children'],
+    terrain: [
+      city,
+      village,
+    ]
+  },
+  {
+    title: 'Jungle Cove',
+    location: 'https://michaelghelfi.bandcamp.com/track/cove',
+    tags: ['cove', 'water', 'waves', 'animals', 'night'],
+    terrain: [
+      coast,
+      jungle,
+    ]
+  },
+  {
+    title: 'Haunted Coast',
+    location: 'https://michaelghelfi.bandcamp.com/track/on-the-haunted-shore',
+    tags: ['coast', 'haunted', 'magic', 'night', 'creepy'],
+    terrain: [
+      coast,
+      ocean,
+    ]
+  },
+  {
+    title: 'Blacksmith\'s Forge',
+    location: 'https://michaelghelfi.bandcamp.com/track/blacksmith',
+    tags: ['blacksmith', 'torches', 'forge'],
+    terrain: [
+      city,
+      village,
+    ]
+  },
+  {
+    title: 'Winter Coast',
+    location: 'https://michaelghelfi.bandcamp.com/track/frozen-coast',
+    tags: ['windy', 'waves', 'creepy'],
+    terrain: [
+      winter,
+      coast,
+      ocean,
+    ]
+  },
+  {
+    title: 'Windy Shack',
+    location: 'https://michaelghelfi.bandcamp.com/track/abandoned-shack',
+    tags: ['windy', 'people'],
+    terrain: [
+      coast,
+      desert,
+      forest,
+      mountains,
+      plains,
+      winter,
+    ]
+  },
+  {
+    title: 'Beach',
+    location: 'https://michaelghelfi.bandcamp.com/track/on-the-shore',
+    tags: ['coast', 'beach', 'peaceful', 'windy', 'gulls'],
+    terrain: [
+      coast,
+    ]
+  },
+  {
+    title: 'Sailing Ship',
+    location: 'https://michaelghelfi.bandcamp.com/track/sailing-ship',
+    tags: ['chains', 'wind', 'creaking', 'windy', 'water'],
+    terrain: [
+      ocean,
+    ]
+  },
+  {
+    title: 'Harbor',
+    location: 'https://michaelghelfi.bandcamp.com/track/harbour',
+    tags: ['coast', 'harbor', 'ships', 'creaking', 'waves', 'gulls'],
+    terrain: [
+      coast,
+      freshwater,
+      ocean,
+    ]
+  },
+  {
+    title: 'Ship in Storm',
+    location: 'https://michaelghelfi.bandcamp.com/track/ship-in-storms',
+    tags: ['coast', 'harbor', 'ships', 'creaking', 'waves', 'storm'],
+    terrain: [
+      coast,
+      ocean,
+    ]
+  },
+  {
+    title: 'Ship\'s Cabin (by land, storm)',
+    location: 'https://www.youtube.com/watch?v=Ft-lJyu_nuY',
+    tags: ['coast', 'harbor', 'ships', 'creaking', 'waves', 'birds', 'storm'],
+    terrain: [
+      coast,
+    ]
+  },
+  {
     title: 'Lively Market',
     location: 'https://www.youtube.com/watch?v=gcPSA3sUilc',
     tags: ['town', 'harbor', 'music', 'drums', 'loud', 'shouting'],
@@ -49,6 +425,39 @@ export default [
       city,
       village,
     ]
+  },
+  {
+    title: 'Desert Canyon',
+    location: 'https://michaelghelfi.bandcamp.com/track/desert-canyon-2',
+    tags: ['desert', 'wind', 'birds'],
+    terrain: [
+      coast,
+      desert,
+      forest,
+      jungle,
+      mountains,
+    ]
+  },
+  {
+    title: 'Volcano',
+    location: 'https://michaelghelfi.bandcamp.com/track/volcano-temple',
+    tags: ['desert', 'wind', 'birds'],
+    terrain: [
+      exotic,
+    ]
+  },
+  {
+    title: 'Deep Forge',
+    location: 'https://www.youtube.com/watch?v=gS0sNL82Tvk',
+    tags: ['hammers', 'dwarves', placeNames.durukhaaz, 'city', 'underground'],
+    terrain: [
+      caves,
+      exotic,
+    ],
+    places: [{
+      name: placeNames.durukhaaz,
+      trackTitle: 'The Clanging Cavern',
+    }]
   },
   {
     title: 'Town',
@@ -61,5 +470,49 @@ export default [
       name: placeNames.durukhaaz,
       trackTitle: 'The Streets',
     }]
+  },
+  {
+    title: 'Marketplace',
+    location: 'https://www.youtube.com/watch?v=UlbxDQMo1o4',
+    tags: ['people', 'market', 'busy', 'dogs'],
+    terrain: [
+      city,
+    ],
+    places: [{
+      name: placeNames.durukhaaz,
+      trackTitle: 'The Merchantry',
+    }]
+  },
+  {
+    title: 'Mushroom Forest (underground)',
+    location: 'https://www.youtube.com/watch?v=dzV3o8zr5Hw',
+    tags: ['magic', 'calm', 'mushrooms', 'underground'],
+    terrain: [
+      caves,
+    ],
+    places: [{
+      name: placeNames.durukhaaz,
+      trackTitle: 'The Forests',
+    }]
+  },
+  {
+    title: 'Mine',
+    location: 'https://www.youtube.com/watch?v=F6_ZuJSWQ4c',
+    tags: ['mining', 'work', 'caves', 'underground'],
+    terrain: [
+      caves,
+    ],
+    places: [{
+      name: placeNames.durukhaaz,
+      trackTitle: 'The Mines', 
+    }]
+  },
+  {
+    title: 'Desert Temple',
+    location: 'https://www.youtube.com/watch?v=Zost8sfrQA8',
+    tags: ['creepy', 'ruins', 'wind', 'underground'],
+    terrain: [
+      caves,
+    ],
   },
 ]
--- src/data/aspectData/ambiences/moodTracks.js
+++ src/data/aspectData/ambiences/moodTracks.js
@@ -6,7 +6,12 @@ export default {
     titleColor: colors.battleRed,
     trackObjects: [
       {
-        title: 'Taiko Drums (1:00)',
+        title: 'Battle - Skyrim (1:00)',
+        location: 'https://www.youtube.com/watch?v=yIMy3pOeo34',
+        tags: ['skyrim', 'battle', 'serious'],
+      },
+      {
+        title: 'Taiko Drums - clacks (1:00)',
         location: 'https://www.youtube.com/watch?v=1nq8G4BT7-w ',
         tags: ['skyrim', 'battle', 'serious', 'mix'],
         
@@ -16,15 +21,36 @@ export default {
         location: 'https://www.youtube.com/watch?v=47UqaBordWE',
         tags: ['skyrim', 'battle', 'serious', 'mix'],
       },
+      {
+        title: 'Creepy Fight',
+        location: 'https://www.youtube.com/watch?v=MI2wq39BPFo ',
+        tags: ['creepy', 'unsettling', 'fear', 'anxiety'],
+      },
     ],
   },
   'Exploring': {
     titleColor: colors.exploringTeal,
     trackObjects: [
       {
-        title: 'Exploring Mix (0:58)',
+        title: 'Exploring Mix 1 (0:58)',
         location: 'https://www.youtube.com/watch?v=S4ADbLPLmN8',
         tags: ['hooves', 'horseback', 'birdsong', 'walk'],
+      },
+      {
+        title: 'Exploring Mix 2 (0:58)',
+        location: 'https://www.youtube.com/watch?v=A8qMyBWZNw0',
+        tags: ['hooves', 'horseback', 'birdsong', 'walk'],
+      },
+      {
+        title: 'Night - Skyrim (1:00)',
+        location: 'https://www.youtube.com/watch?v=aK4JSwhdcdE',
+        tags: ['skyrim', 'battle', 'serious'],
+        terrain: [],
+      },
+      {
+        title: 'Moonlight',
+        location: 'https://www.youtube.com/watch?v=tZBV2JJ20Hw',
+        tags: ['moonlight', 'night', 'birds', 'magic', 'peaceful'],
       },
       {
         title: 'Enchanted Lands',
@@ -37,34 +63,84 @@ export default {
     titleColor: colors.puzzleOrange,
     trackObjects: [
       {
+        title: 'Suspense, torches',
+        location: 'https://www.youtube.com/watch?v=6AlsnSQZP8I',
+        tags: ['ruins', 'fire', 'quiet', 'peaceful'],
+      },
+      {
+        title: 'Puzzle Room (Creepy)',
+        location: 'https://www.youtube.com/watch?v=IA0_cxItj9s',
+        tags: [
+          'crystal', 'cave', 'underground', 'magical', 'wonder', 'puzzle', 'whispers'
+        ],
+      },
+      {
         title: 'Taiko Drums - steady (1:11)',
         location: 'https://www.youtube.com/watch?v=KenLU_OtXsc',
         tags: ['puzzle', 'calm', 'thunder', 'interesting'],
       },
     ],
   },
-  // 'Magic': {
-  //   titleColor: colors.magicPink,
-  //   trackObjects: [
-      
-  //   ]
-  // },
+  'Magic': {
+    titleColor: colors.magicPink,
+    trackObjects: [
+      {
+        title: 'Moonlight',
+        location: 'https://www.youtube.com/watch?v=tZBV2JJ20Hw',
+        tags: ['moonlight', 'night', 'birds', 'magic', 'peaceful'],
+      },
+      {
+        title: 'Crystal Cave',
+        location: 'https://www.youtube.com/watch?v=-ck5wXXD-_4',
+        tags: [
+          'crystal', 'cave', 'underground', 'magical', 'wonder',
+        ],
+      },
+    ]
+  },
   'Tension': {
     titleColor: colors.creepyPurple,
     trackObjects: [
+      {
+        title: 'Slow Dread',
+        location: 'https://www.youtube.com/watch?v=3radp0y5FFs',
+        tags: ['creepy', 'unsettling', 'fear', 'anxiety'],
+      },
+      {
+        title: 'Puzzle Room',
+        location: 'https://www.youtube.com/watch?v=IA0_cxItj9s',
+        tags: [
+          'crystal', 'cave', 'underground', 'magical', 'wonder', 'puzzle', 'whispers'
+        ],
+      },
       {
         title: 'Creepy Fight',
         location: 'https://www.youtube.com/watch?v=MI2wq39BPFo ',
         tags: ['creepy', 'unsettling', 'fear', 'anxiety'],
       },
+      {
+        title: 'Creepy Dungeon',
+        location: 'https://www.youtube.com/watch?v=j6rCcTTOs1I',
+        tags: ['dark', 'creepy'],
+      },
     ],
   },
-  // 'Weather': {
-  //   titleColor: colors.weatherBlue,
-  //   trackObjects: [
-
-  //   ]
-  // },
+  'Weather': {
+    titleColor: colors.weatherBlue,
+    trackObjects: [
+      {
+        title: 'Rain',
+        location: 'https://www.youtube.com/watch?v=fGRh_hIpDt4',
+        tags: ['rain',],
+        excludeFrom: [terrainTypeNames.winter],
+      },
+      {
+        title: 'Wind',
+        location: 'https://www.youtube.com/watch?v=1zCDGPtDR8Q',
+        tags: ['wind', 'storm'],
+      },
+    ]
+  },
   'Tavern': {
     titleColor: colors.tavernSalmon,
     trackObjects: [
@@ -72,6 +148,11 @@ export default {
         title: 'Music: Tavern (4:28)',
         location: 'https://www.youtube.com/watch?v=hWPPD5ww0eA',
         tags: ['tavern', 'inn', 'bard', 'music'],
+      },
+      {
+        title: 'Shady Tavern',
+        location: 'https://www.youtube.com/watch?v=Ag8sbpNXBEQ',
+        tags: ['tavern',],
       },
     ],
   }
