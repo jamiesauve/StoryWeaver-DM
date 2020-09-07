169d55cb5751e046dbeb85676da192c70f518b44
Jamie Sauve <jamiesauve@zohomail.com>
2020-09-07 16:31:01 -0500

    Make a way to access the database that should work in production
    - before this, I was starting up a local server in the background,
    and sending API calls to that to interact with the database. But
    when it was packaged for production, that server wouldn't work
    correctly.
    - To fix this, use Electron's IPC communication to emit and respond
    to events between Electron's main and renderer processes, since the
    main process can access the database. I will remove the local
    server once I have everything connecting properly using the IPC
    strategy.

--- public/electron.js
+++ public/electron.js
@@ -1,14 +1,33 @@
 const electron = require('electron');
 const app = electron.app;
+const ipcMain = electron.ipcMain;
 const BrowserWindow = electron.BrowserWindow;
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 const path = require('path');
 const isDev = require('electron-is-dev');
 
+const databaseApi = require('../pesudoServer/api/controller/routes')
+
+ipcMain.on('dbRequest', async (event, request) => {
+  console.log('IN MAIN, MESSAGE RECEIVED:', request)
+  const response = await databaseApi(request)
+  event.reply('dbResponse', response)
+})
+
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false,
+      /**
+       * import native Node modules explicitly in this file; something about Create-React-App and webpack mess
+       * with Electron's ability to provide these directly.
+       * See the comment by HemalR here: https://github.com/electron/electron/issues/9920
+       */
+      preload: __dirname + '/preload.js'
+    },
+  });
   mainWindow.maximize();
   mainWindow.loadURL(isDev ? 'http://localhost:4203' : `file://${path.join(__dirname, '../build/index.html')}`);
   if (isDev) {
@@ -18,7 +37,7 @@ function createWindow() {
 
     installExtension(REACT_DEVELOPER_TOOLS)
     .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err)); 
+    .catch((err) => console.log('An error occurred: ', err));
   }
   mainWindow.on('closed', () => mainWindow = null);
 }
