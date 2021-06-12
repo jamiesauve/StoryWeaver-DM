5212d96042dbd57f1e8a6a09b5c92d26f2a5e505
Jamie Sauve <jamiesauve@zohomail.com>
2021-06-12 09:27:01 -0500

    Rename /pseudoServer to /server since it is now a real server

--- server/api/dbConnect.js
+++ server/api/dbConnect.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose()
 
 // this is not a relative path from this file - I think it needs to be relative to the package.json, but I'm not sure.
-const db = new sqlite3.Database('./pseudoServer/database/story_weaver.db', sqlite3.OPEN_READWRITE, err => {
+const db = new sqlite3.Database('./server/database/story_weaver.db', sqlite3.OPEN_READWRITE, err => {
   if (err) console.log('error during db connection:', err)
   else console.log('successfully connected to the db.')
 })
